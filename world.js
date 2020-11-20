eacc6e302fdc8e04f6827fd73e046fccd5e154c5
shan2812 <shanicereid406@gmail.com>
2020-11-19 23:44:48 -0500

    Edited starter files

--- world.js
+++ world.js
@@ -9,5 +9,19 @@ function countrySearch(){
     };
     var url = "world.php?country="+query;
     httprequest.open("GET",url,true);
-    httprequest.send("");
+    httprequest.send();
 }
+function citySearch(){
+    var query = document.getElementById("country").value;
+    var httprequest = new XMLHttpRequest();
+    httprequest.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200){
+            document.getElementById("result").innerHTML=this.responseText;
+            alert(this.responseText);
+        }
+    };
+    var url = "world.php?country="+query;
+    httprequest.open("GET",url,true);
+    httprequest.send();
+}
+
--- world.php
+++ world.php
@@ -12,7 +12,17 @@ if (isset($country) || !empty($country)){
   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
   echo '<ul>';
  foreach ($results as $row){
-  echo '<li>'. $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+  echo '<li>'. $row['name'] . $row['continent'] . $row['independence'] $row['head_of_state'] . '</li>';
+ }
+ echo '</ul>';
+}
+if (isset($country) || !empty($country)){
+  $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+  $stmt = $conn->query("SELECT name FROM countries JOIN cities");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  echo '<ul>';
+ foreach ($results as $row){
+  echo '<li>'. $row['name'] . $row['district'] . $row['population'] . '</li>';
  }
  echo '</ul>';
 }
